e091c50e2038e35575f1190d711d9c5e8cfbc993
et.touil <et.touil505@gmail.com>
2024-02-20 22:06:01 +0100

    add  Insertion sort function prototype

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,7 +2,7 @@
 
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers in ascending
- * order using the Insertion sort algorithm
+ *                       order using the Insertion sort algorithm
  * @list: Double pointer to the head of the list
  */
 void insertion_sort_list(listint_t **list)
@@ -34,3 +34,4 @@ void insertion_sort_list(listint_t **list)
         curr = curr->next;
     }
 }
+
