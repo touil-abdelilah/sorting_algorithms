854f142694ce204ee6b42e8d89661f5021a7344f
et.touil <et.touil505@gmail.com>
2024-02-20 22:11:06 +0100

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,10 @@
 #include "sort.h"
-
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
+ *                       order using the Insertion sort algorithm
+ * @list: Double pointer to the head of the list
+ *
+ */
 
 void insertion_sort_list(listint_t **list)
 {
