51d7aec7b9730fbc5379213cc5e9bcc12deea75e
et.touil <et.touil505@gmail.com>
2024-02-20 22:31:10 +0100

    Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,80 @@
+#include "sort.h"
+/**
+ * swap - Swaps two integers in an array
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/**
+ * lomuto_partition - Partitions the array using the Lomuto scheme
+ * @array: The array to be partitioned
+ * @low: The starting index of the partition
+ * @high: The ending index of the partition
+ * @size: The size of the array
+ *
+ * Return: The index of the pivot element after partitioning
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+    int pivot = array[high];
+    int i = low - 1;
+    int j;
+
+    for (j = low; j <= high - 1; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            if (i != j)
+            {
+                swap(&array[i], &array[j]);
+                print_array(array, size);
+            }
+        }
+    }
+    if (array[i + 1] > array[high])
+    {
+        swap(&array[i + 1], &array[high]);
+        print_array(array, size);
+    }
+    return (i + 1);
+}
+
+/**
+ * quicksort - Sorts an array of integers using the Quick sort algorithm
+ * @array: The array to be sorted
+ * @low: The starting index of the array
+ * @high: The ending index of the array
+ * @size: The size of the array
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+    int pivot;
+
+    if (low < high)
+    {
+        pivot = lomuto_partition(array, low, high, size);
+        quicksort(array, low, pivot - 1, size);
+        quicksort(array, pivot + 1, high, size);
+    }
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using
+ *              the Quick sort algorithm
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ */
+void quick_sort(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
+        return;
+
+    quicksort(array, 0, size - 1, size);
+}
